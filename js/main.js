e677a478440400252ab9c06a29fcf33ca5ff732c
GhassenData <56271353+GhassenData@users.noreply.github.com>
2019-11-22 20:06:03 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+﻿
+
+ function txt1(){
+     document.getElementById('z-txt').style.fontWeight='bold'
+ }
+
+ function txt2(){
+    document.getElementById('z-txt').style.fontStyle='italic'
+ }
+
+ function txt3(){
+    document.getElementById('z-txt').style.textDecoration ='underline'
+ }
+
+ function txt4(){
+ document.getElementById('z-txt').style.fontSize= document.getElementById('txt4').value
+ }
+
+ function txt5(){
+    document.getElementById('z-txt').style.fontFamily= document.getElementById('txt5').value
+    }
+   
+   
+
+
+
+
+
+
+
+
